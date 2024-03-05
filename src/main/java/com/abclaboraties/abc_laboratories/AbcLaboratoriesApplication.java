e7c698ad5495a2fd4c1dbe002073a48f55cd4052
rajeewUvind <uvindu@jwareautomation.com>
2024-03-05 22:40:13 +0530

    first commit

---
+++ src/main/java/com/abclaboraties/abc_laboratories/AbcLaboratoriesApplication.java
@@ -0,0 +1,13 @@
+package com.abclaboraties.abc_laboratories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcLaboratoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcLaboratoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abclaboraties/abc_laboratories/AbcLaboratoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abclaboraties.abc_laboratories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcLaboratoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
